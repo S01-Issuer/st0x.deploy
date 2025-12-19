f4be418d5a0b37f7322f9d99990572bcee39f7f3
thedavidmeister <thedavidmeister@gmail.com>
2025-12-19 13:35:02 +0400

    rainlang.eth initial deployer

--- src/lib/LibProdDeploy.sol
+++ src/lib/LibProdDeploy.sol
@@ -3,5 +3,6 @@
 pragma solidity ^0.8.25;
 
 library LibProdDeploy {
-    address constant BEACON_INIITAL_OWNER = address(0x01);
+    /// rainlang.eth
+    address constant BEACON_INIITAL_OWNER = address(0x8E4bdeec7CEB9570D440676345dA1dCe10329f5b);
 }
