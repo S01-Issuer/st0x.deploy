409d4774e1886c940bce993997e1c9e71275eb06
thedavidmeister <thedavidmeister@gmail.com>
2026-01-19 18:02:24 +0400

    stox wrapped token vault

---
+++ src/concrete/StoxWrappedTokenVault.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
+pragma solidity =0.8.25;
+
+import {ERC4626Upgradeable} from
+    "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
+
+contract StoxWrappedTokenVault is ERC4626Upgradeable {}
