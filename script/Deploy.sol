3bfdb9ca84627c8f9872e27bbb9f2404eb125bda
thedavidmeister <thedavidmeister@gmail.com>
2026-01-20 17:43:08 +0400

    docs

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -18,14 +18,22 @@ import {
 import {StoxWrappedTokenVault} from "src/concrete/StoxWrappedTokenVault.sol";
 import {StoxUnifiedDeployer} from "src/concrete/deploy/StoxUnifiedDeployer.sol";
 
+/// @dev The deployment suite name for the offchain asset receipt vault beacon
+/// set.
 bytes32 constant DEPLOYMENT_SUITE_OFFCHAIN_ASSET_RECEIPT_VAULT_BEACON_SET =
     keccak256("offchain-asset-receipt-vault-beacon-set");
 
+/// @dev The deployment suite name for the wrapped token vault beacon set.
 bytes32 constant DEPLOYMENT_SUITE_WRAPPED_TOKEN_VAULT_BEACON_SET = keccak256("wrapped-token-vault-beacon-set");
 
+/// @dev The deployment suite name for the unified deployer.
 bytes32 constant DEPLOYMENT_SUITE_UNIFIED_DEPLOYER = keccak256("unified-deployer");
 
 contract Deploy is Script {
+    /// @notice Deploys the OffchainAssetReceiptVaultBeaconSetDeployer contract.
+    /// Creates both StoxReceipt and StoxReceiptVault anew for the initial
+    /// implementations. Initial owner is set to the BEACON_INIITAL_OWNER
+    /// constant in LibProdDeploy.
     function deployOffchainAssetReceiptVaultBeaconSet(uint256 deploymentKey) internal {
         vm.startBroadcast(deploymentKey);
 
@@ -40,6 +48,10 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 
+    /// @notice Deploys the StoxWrappedTokenVaultBeaconSetDeployer contract.
+    /// Creates a StoxWrappedTokenVault anew for the initial implementation.
+    /// Initial owner is set to the BEACON_INIITAL_OWNER constant in
+    /// LibProdDeploy.
     function deployWrappedTokenVaultBeaconSet(uint256 deploymentKey) internal {
         vm.startBroadcast(deploymentKey);
 
@@ -53,6 +65,7 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 
+    /// @notice Deploys the StoxUnifiedDeployer contract.
     function deployUnifiedDeployer(uint256 deploymentKey) internal {
         vm.startBroadcast(deploymentKey);
 
@@ -61,6 +74,9 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 
+    /// @notice Entry point for the deployment script. Dispatches to the
+    /// appropriate deployment function based on the DEPLOYMENT_SUITE environment
+    /// variable.
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
         bytes32 suite = keccak256(bytes(vm.envString("DEPLOYMENT_SUITE")));
